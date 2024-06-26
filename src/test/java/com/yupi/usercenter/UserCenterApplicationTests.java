88923eb0e98a8097590b0a1b1310ee8b936c8555
zhangweibao <1414626899@qq.com>
2024-06-26 23:11:54 +0800

    initial commit

---
+++ src/test/java/com/yupi/usercenter/UserCenterApplicationTests.java
@@ -0,0 +1,46 @@
+package com.yupi.usercenter;
+
+import com.yupi.usercenter.model.domain.User;
+import com.yupi.usercenter.service.UserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.DigestUtils;
+
+import javax.annotation.Resource;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 启动类测试
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@SpringBootTest
+class UserCenterApplicationTests {
+
+    // https://yupi.icu/
+
+    @Test
+    void testDigest() throws NoSuchAlgorithmException {
+        String newPassword = DigestUtils.md5DigestAsHex(("abcd" + "mypassword").getBytes());
+        System.out.println(newPassword);
+    }
+
+    @Resource
+    private UserService userService;
+
+    @Test
+    void testTags(){
+        List<User> users = userService.searchUserByTags(Arrays.asList("java","开发"));
+        System.out.println(users);
+        Assertions.assertNotEquals(users.size(),0);
+    }
+
+}
+
+// [编程知识星球](https://yupi.icu) 连接万名编程爱好者，一起优秀！20000+ 小伙伴交流分享、100+ 各方向编程交流群、40+ 大厂嘉宾一对一答疑、4000+ 编程问答参考
